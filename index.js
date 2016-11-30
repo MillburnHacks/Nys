c3313cc6b64669b4146a4736e58cd787c551edf8
Abhinav Madahar <abhinavmadahar@gmail.com>
2016-11-29 19:10:05 -0500

    Remove hard-coded firebase credentials

--- index.js
+++ index.js
@@ -1,43 +1,50 @@
 // all teams are saved in localStorage
 // localStorage doesn't support arrays, so we have to convert the teams array to a string and back
-// these functions make it easier to manage teams without directly calling localStorage
 const getTeams = () => JSON.parse(localStorage.getItem("teams"));
+
 const hasTeam = (name) => getTeams().map(team => team.name).indexOf(name) !== -1;
-const isValidTeam = (team) => team.hasOwnProperty("name") && team.hasOwnProperty("notes");
+
+// this makes it easy to validate a team
+const teamFields = ["name", "notes", "autonPoints", "auton"];
+const isValidTeam = (team) => teamFields
+  .map(property => team.hasOwnProperty(property))
+  .reduce((isValidSoFar, hasProp) => isValidSoFar && hasProp);
+
+// only adds the team if the team is valid and is not already in localStorage
 const addTeam = (team) => {
   // add the team
-  if (!hasTeam(team.name)) {
+  if (isValidTeam(team) && !hasTeam(team.name)) {
     localStorage.setItem("teams", JSON.stringify(getTeams().concat(team)));
   }
-
-  // remove any teams that don't have both a name and notes
-  const teams = getTeams().filter(isValidTeam);
-  localStorage.setItem("teams", JSON.stringify(teams));
 };
+
 const removeTeam = (name) => {
   const teamsWithoutTeam = getTeams().filter(t => t.name !== name);
   localStorage.setItem("teams", JSON.stringify(teamsWithoutTeam));
+
+  // try to sync to firebase
+  // if it can't sync, just fail silently
   if (firebase) {
-    firebase.database().ref(`team/${name}`).set(null);
+    try {
+      firebase.database().ref(`team/${name}`).set(null);
+    } catch (error) {}
   }
 };
+
 const uploadTeamToFirebase = (team) => firebase.database().ref(`team/${team.name}`).set(team);
 const uploadToFirebase = () => getTeams.forEach(uploadTeamToFirebase);
 
 // initialize the teams value by making sure that it is set to an array
 localStorage.setItem("teams", localStorage.getItem("teams") || "[]");
 
-// show all the currently saved teams in #teams
+// remove any invalid teams before displaying them
+getTeams().filter(team => !isValidTeam(team)).forEach(team => removeTeam(team.name));
+
 const renderTeams = () => {
   document.getElementById("teams").innerHTML = ""; // clear the teams
 
-  const teams = getTeams();
-  if (teams.length > 0) {
-    document.getElementById("teams").innerHTML = "<h2 id=\"team-title\">Teams</h2>";
-  }
-  for (let i = 1; i < teams.length; i++) {
+  for (let team of getTeams()) {
     const teamNode = document.createElement("div");
-    const team = teams[i];
     teamNode.innerHTML = `
       <h3>${team.name}</h3>
       <p>${team.notes}</p>
@@ -57,38 +64,45 @@ const renderTeams = () => {
   }
 };
 
+renderTeams();
+
 // save the team entered when save button is pressed
 document.getElementById("save").addEventListener("click", (event) => {
   event.preventDefault();
-  const name = document.getElementById("name").value;
-  const notes = document.getElementById("notes").value;
-  const autonPoints = document.getElementById("autonPoints").value;
-  const auton = document.getElementById("auton").checked;
-  if (name) {
-    const team = { name, notes, autonPoints, auton };
+  const team = {};
+  for (let field of teamFields) {
+    team[field] = document.getElementById(field).value;
+  }
+  if (isValidTeam(team)) {
     addTeam(team);
     uploadTeamToFirebase(team);
     renderTeams();
   }
 });
 
-renderTeams();
+const sync = () => {
+  const firebaseAPIKey = localStorage.getItem("firebase-api-key");
+  const firebaseProjectName = localStorage.getItem("firebase-project-name").toLowerCase();
 
-// handle automatic syncing
-if (firebase) {
+  if (!firebase || !firebaseAPIKey || !firebaseProjectName) {
+    return;
+  }
+
   firebase.initializeApp({
-    apiKey: "AIzaSyCinnVb0Y6eXw9ttyspV32NJnugRvQtbmM",
-    authDomain: "nys-vex.firebaseapp.com",
-    databaseURL: "https://nys-vex.firebaseio.com",
-    storageBucket: "nys-vex.appspot.com",
-    messagingSenderId: "40799368085"
+    apiKey: firebaseAPIKey,
+    authDomain: `${firebaseProjectName}.firebaseapp.com`,
+    databaseURL: `https://${firebaseProjectName}.firebaseio.com`,
+    storageBucket: `${firebaseProjectName}.appspot.com`,
+    messagingSenderId: "805247801529"
   });
 
   // download any new teams and display them
   const teams = firebase.database().ref("team").on("value", (teams) => {
+    document.getElementById("firebase-sync-message").innerHTML =
+      `<strong>Firebase connected to ${firebaseProjectName}</strong>`;
     for (let teamName in teams.val()) {
       const team = teams.val()[teamName];
-      if (!hasTeam(team.name)) {
+      if (!hasTeam(team.name) && isValidTeam(team)) {
         addTeam(team);
       }
     }
@@ -98,3 +112,15 @@ if (firebase) {
   // upload new teams
   getTeams().forEach(team => firebase.database().ref(`team/${team.name}`).set(team));
 }
+
+if (firebase) {
+  sync();
+}
+
+document.getElementById("firebase-sync-save").addEventListener("click", (event) => {
+  event.preventDefault();
+  for (let key of ["firebase-api-key", "firebase-project-name"]) {
+    localStorage.setItem(key, document.getElementById(key).value);
+  }
+  sync();
+});
